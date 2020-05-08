d70870abd02997b23d5961e18d80278326aa7588
João Lima <jpalvesl101@gmail.com>
2020-05-08 05:32:38 +0000

    fazendo com que as pastas sejam carregadas do proprio celular

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
+import { AsyncStorage } from 'react-native';
 
 import { Container, List, ItemList, ItemContent, City, State, AddButton } from './styles';
 
 function HomeScreen() {
+  const [folders, setFolders] = useState([]);
+
   const navigation = useNavigation();
+
+  useEffect(() => {
+    async function LoadFolders() {
+      try {
+        const data = await AsyncStorage.getItem('folders')
+        if (data !== null) {
+          setFolders(JSON.parse(data))
+        } 
+        else alert('Não há pastas') 
+        
+      } catch (error) {
+        alert('Erro ao carregar pastas, tente novamente')
+      }
+    }
+
+    LoadFolders()
+    console.log(folders)
+  }, [])
 
   function NavigateToCreateFolder() {
     navigation.navigate('CreateFolder')
@@ -14,14 +35,14 @@ function HomeScreen() {
   return (
     <Container>
       <List 
-        data={[5,66,321,55555,555555555 ]}
-        keyExtractor={value => String(value)}
+        data={folders}
+        keyExtractor={(value) => String(value.city+value.state)}
         showsVerticalScrollIndicator={false}
-        renderItem={({ item: value }) => (
+        renderItem={({ item: folder }) => (
           <ItemList onPress={() => navigation.navigate('FolderInside')}>
             <ItemContent>
-              <City>Cidade {value}</City>
-              <State>PB</State>
+              <City>{folder.city}</City>
+              <State>{folder.state}</State>
             </ItemContent>
           </ItemList>
           )
