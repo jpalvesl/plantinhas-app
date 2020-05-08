2f8dd8f0b86541755eb850ba48ee2292928b2f75
João Lima <jpalvesl101@gmail.com>
2020-05-08 06:20:23 +0000

    criando a logica para adiçao de novas pastas

--- src/screens/CreateFolderScreen/index.js
+++ src/screens/CreateFolderScreen/index.js
@@ -1,18 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
+import { AsyncStorage } from 'react-native';
 
 import { Container, CheckButton, Input, InputView } from './styles';
 
 function CreateFolderScreen() {
   const [city, setCity] = useState('');
   const [state, setState] = useState('');
+  const [folders, setFolders] = useState([]);
 
   const navigation = useNavigation();
 
-  function addFolder() {
-    alert('Por hora ainda não cria a pasta')
-    navigation.navigate('Home')
+  useEffect(() => {
+    async function LoadFolders() {
+      try {
+        const data = await AsyncStorage.getItem('folders')
+        if (data !== null) {
+          setFolders(JSON.parse(data))
+        }         
+      } catch (error) {
+        alert('Erro ao carregar pastas, tente novamente')
+      }
+    }
+
+    LoadFolders()
+  }, [])
+
+  function folderVerification(folders, fold) {
+    const result = folders.filter((element) => (fold.state === element.state && fold.city === element.city))
+     // caso nao exista nenhuma pasta igual o valor vai ser true 
+    return !result.length
+  }
+
+  async function addFolder() {
+    if (folderVerification(folders, {city, state})) {
+      try {
+        await AsyncStorage.setItem(
+          'folders',
+          JSON.stringify([...folders, {city, state}])
+        )
+      } catch (error) {
+        console.log('error')
+      }
+      navigation.reset({
+        index: 0,
+        routes: [{ name: 'Home' }],
+      });
+    }
+    else {
+      alert('Já existe uma pasta com esses nomes')
+    }
   }
 
   return (
