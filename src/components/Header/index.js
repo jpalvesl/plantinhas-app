a79c05a382ebdd6d78a22749ca6d722bade9c759
João Lima <jpalvesl101@gmail.com>
2020-08-12 19:51:40 +0000

    adicionando a funcao de voltar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Ionicons } from '@expo/vector-icons';
 
 import { Container, BackButton, Title, Content } from './styles';
+import { useNavigation } from '@react-navigation/native';
 
 function Header({ title, hasGoBack, rightButton }) {
+  const { goBack } = useNavigation()
+
   return (
     <Container>
       <Content>
         {hasGoBack && (
-          <BackButton onPress={() => {}}>
+          <BackButton onPress={goBack}>
             <Ionicons name="ios-arrow-round-back" size={30} color="#000" />
           </BackButton> 
         )}
